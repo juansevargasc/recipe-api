20249f0e74c5ca3b0b3188a224a1cc8d84fd37fa
Juanse Vargas MBP16 <jsvargasc@unal.edu.co>
2023-06-28 11:59:20 -0500

    :white_check_mark: Adding files for tests and mock functions

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Add two numbers together"""
+    return x + y
+
+def substract(x, y):
+    """Substract x from y and return value"""
+    return y - x
