35a108b8e4f2e58e03d9caa4bc733b1e4e0be9f3
Juanse Vargas MBP16 <jsvargasc@unal.edu.co>
2023-06-30 19:27:52 -0500

    :card_file_box: Database tests and functions.
    Database tests using patch, and database wait_for_db method to wait for the Datbase from set as default to start.

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,17 +5,19 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
+
     def test_add_numbers(self):
         """Test adding two numbers together."""
-        
+
         res = calc.add(3, 8)
-        
+
         self.assertEqual(res, 11)
-        
+
     def test_substract_numbers(self):
         """Test substracts two numbers"""
         res = calc.substract(10, 15)
-        
+
         self.assertEqual(res, 5)
